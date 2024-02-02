f31e4db5d8eed24e50d7ca36aa617bbc7824cbcd
Stellacore <dave@stellacore.com>
2024-02-01 18:29:48 -0700

    README.md,include/ removed trailing spaces from header text

--- include/OriMania.hpp
+++ include/OriMania.hpp
@@ -1,18 +1,18 @@
-// 
+//
 // MIT License
-// 
+//
 // Copyright (c) 2023 Stellacore Corporation
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-// 
+//
 
 
 #ifndef OriMania__INCL_
@@ -28,14 +28,14 @@
 
 /*! \file
  * \brief Top level include file for all of OriMania utilities.
- * 
+ *
  */
 
 /*! \brief Main namespace for OriMania project software.
  *
  * Example:
  * \snippet test_cmake.cpp DoxyExample01
- * 
+ *
  */
 namespace om
 {
