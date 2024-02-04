9379a6b2aaca8a35a0ae6c3054ae4afa69219eef
Stellacore <dave@stellacore.com>
2024-02-04 13:42:14 -0700

    test/test_Analyse.cpp - add a few lines of code that utilize Rigibra package functions for build testing

--- test/test_Analyse.cpp
+++ test/test_Analyse.cpp
@@ -32,11 +32,14 @@
 
 #include "OriMania.hpp"
 
+#include <Rigibra>
+
 #include <algorithm>
 #include <array>
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
+#include <map>
 #include <set>
 #include <sstream>
 #include <vector>
@@ -44,6 +47,8 @@
 
 namespace
 {
+	using SenKey = std::string;
+
 	//! Transformation convention: translate then rotate or v.v.
 	enum OrderTR
 	{
@@ -368,6 +373,53 @@ namespace
 			);
 	}
 
+	using Ori = rigibra::Transform;
+	std::map<SenKey, Ori>
+	simEOs
+		()
+	{
+		std::map<SenKey, Ori> oris;
+		oris.emplace_hint
+			( oris.end()
+			, std::make_pair(SenKey("fake1"), rigibra::identity<Ori>())
+			);
+		oris.emplace_hint
+			( oris.end()
+			, std::make_pair(SenKey("fake2"), rigibra::identity<Ori>())
+			);
+		oris.emplace_hint
+			( oris.end()
+			, std::make_pair(SenKey("fake3"), rigibra::identity<Ori>())
+			);
+		return oris;
+	};
+
+	/*! \brief All combinations of (non trivial) relative orientations.
+	 *
+	 *
+	 */
+	std::map<std::pair<SenKey, SenKey>, Ori>
+	relativeOrientationBetweens
+		( std::map<SenKey, Ori> const & senWrtInds
+		)
+	{
+		std::map<std::pair<SenKey, SenKey>, Ori> roInds;
+		for (std::pair<SenKey, Ori> const senWrtInd1 : senWrtInds)
+		{
+			for (std::pair<SenKey, Ori> const senWrtInd2 : senWrtInds)
+			{
+				std::pair<SenKey, SenKey> const keyPair
+					{ senWrtInd1.first, senWrtInd2.first };
+std::cout << "keyPair: " << keyPair.first << ", " << keyPair.second << '\n';
+				Ori const & ori1wR = senWrtInd1.second;
+				Ori const & ori2wR = senWrtInd2.second;
+				Ori const & oriRw1{ inverse(ori1wR) };
+				Ori const & ori2w1{ ori2wR * oriRw1 };
+			}
+		}
+		return roInds;
+	}
+
 	//! Check that number and uniqueness of conventions
 	void
 	testConventions
@@ -417,13 +469,35 @@ namespace
 	{
 		using namespace om;
 
+		//
+		// Simulate sensor data
+		//   1) Individual ori of each sensor in system 'Box' frame
+		//   2) Relative orienation between objects in some arbitrary frame.
+		//
+
 		// simulate arbitrary ExCal orientations
-//		std::vector<QQ> const senWrtBoxs{};
+		std::vector<rigibra::Transform> const senWrtBoxs{};
 
 		// use the ExCal data to generate Independent EO data
-//		std::vector<QQ> const senWrtInds{};
+		std::map<SenKey, rigibra::Transform> const senWrtInds{};
 
-//		std::vector<Convention> conventions{};
+
+		// generate RO pairs
+		std::map<std::pair<SenKey, SenKey>, rigibra::Transform>
+			const roXforms{ relativeOrientationBetweens(senWrtInds) };
+
+		// consider common conventions
+		std::vector<Convention> conventions{};
+
+		// compute BoxRo
+			// candidate for Sen1wBox
+			// candidate for Sen2wBox
+//			BoxRo = Sen2wBox * inverse(Sen1wBox);
+		// IndRo
+//			IndRo = Sen2wInd * inverse(Sen1wInd);
+
+		// compare and assign metric
+//		std::map<CHash, Metric>
 
 
 		// [DoxyExample01]
